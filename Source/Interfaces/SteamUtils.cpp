94a5af113a338bf18085ede6644d9af595ffd528
Convery <Convery10122@gmail.com>
2015-11-13 20:03:32 +0100

    Implemented steam utils.

---
+++ Source/Interfaces/SteamUtils.cpp
@@ -0,0 +1,309 @@
+/*
+    Initial author: (https://github.com/)Convery
+    License: LGPL 3.0
+    Started: 2015-11-13
+    Notes:
+        User independent utilities.
+*/
+
+#define WIN32_LEAN_AND_MEAN
+#include <Windows.h>
+#include "Interface.h"
+#include "..\Macros.h"
+
+#pragma region Methods
+uint32_t GetSecondsSinceAppActive()
+{
+    return (GetTickCount64() - Global::ApplicationStart) / 1000;
+}
+uint32_t GetSecondsSinceComputerActive()
+{
+    return GetTickCount64() / 1000;
+}
+uint32_t GetConnectedUniverse()
+{
+    // k_EUniversePublic
+    return 1;
+}
+uint32_t GetServerRealTime()
+{
+    return time(NULL);
+}
+
+const char *GetIPCountry()
+{
+    static uint64_t Geolocation = 0;
+
+    if (AYRIACLIENT("FetchGeolocation", &Geolocation))
+        return (char *)&Geolocation;
+
+    return "SE";
+}
+bool GetImageSize(int iImage, uint32_t *pnWidth, uint32_t *pnHeight)
+{
+    // We do not handle any image requests.
+    return false;
+}
+bool GetImageRGBA(int iImage, uint8_t *pubDest, int nDestBufferSize)
+{
+    // We do not handle any image requests.
+    return false;
+}
+bool GetCSERIPPort(uint32_t *unIP, uint16_t *usPort)
+{
+    // As we don't connect to steam directly, this is not needed.
+    return false;
+}
+uint8_t GetCurrentBatteryPower()
+{
+    // We are always on AC power.
+    return 255;
+}
+
+uint32_t GetAppID()
+{
+    return Global::ApplicationID;
+}
+void SetOverlayNotificationPosition(uint32_t eNotificationPosition)
+{
+
+}
+bool IsAPICallCompleted(uint64_t hSteamAPICall, bool *pbFailed)
+{
+    return false;
+}
+uint32_t GetAPICallFailureReason(uint64_t hSteamAPICall)
+{
+    return 0;
+}
+bool GetAPICallResult(uint64_t hSteamAPICall, void *pCallback, int cubCallback, int iCallbackExpected, bool *pbFailed)
+{
+    return false;
+}
+void RunFrame()
+{
+
+}
+uint32_t GetIPCCallCount()
+{
+    // Debug information.
+    return 100;
+}
+void SetWarningMessageHook(size_t pFunction)
+{
+
+}
+
+bool IsOverlayEnabled()
+{
+    return false;
+}
+
+bool BOverlayNeedsPresent()
+{
+    return false;
+}
+uint64_t CheckFileSignature(const char *szFileName)
+{
+    return 0;
+}
+bool ShowGamepadTextInput(uint32_t eInputMode, uint32_t eInputLineMode, const char *szText, uint32_t uMaxLength)
+{
+    return false;
+}
+uint32_t GetEnteredGamepadTextLength()
+{
+    return 0;
+}
+bool GetEnteredGamepadTextInput(char *pchValue, uint32_t cchValueMax)
+{
+    return false;
+}
+
+const char *GetSteamUILanguage()
+{
+    uint32_t ApplicationID = Global::ApplicationID;
+    static char Language[64]{};
+
+    if (AYRIADEVELOPER("FetchPlayerLanguage", ApplicationID, Language))
+        return Language;
+
+    return "english";
+}
+bool IsSteamRunningInVR()
+{
+    return false;
+}
+
+#pragma endregion
+
+void INTERFACE_STEAMUTILS001::Initialize()
+{
+    Methods = new void *[4];
+
+    Methods[0] = GetSecondsSinceAppActive;
+    Methods[1] = GetSecondsSinceComputerActive;
+    Methods[2] = GetConnectedUniverse;
+    Methods[3] = GetServerRealTime;
+
+    Initialized = true;
+}
+void INTERFACE_STEAMUTILS002::Initialize()
+{
+    Methods = new void *[8];
+
+    Methods[0] = GetSecondsSinceAppActive;
+    Methods[1] = GetSecondsSinceComputerActive;
+    Methods[2] = GetConnectedUniverse;
+    Methods[3] = GetServerRealTime;
+    Methods[3] = GetIPCountry;
+    Methods[4] = GetImageSize;
+    Methods[5] = GetImageRGBA;
+    Methods[6] = GetCSERIPPort;
+    Methods[7] = GetCurrentBatteryPower;
+
+    Initialized = true;
+}
+void INTERFACE_STEAMUTILS003::Initialize()
+{
+    Methods = new void *[16];
+
+    Methods[0] = GetSecondsSinceAppActive;
+    Methods[1] = GetSecondsSinceComputerActive;
+    Methods[2] = GetConnectedUniverse;
+    Methods[3] = GetServerRealTime;
+    Methods[3] = GetIPCountry;
+    Methods[4] = GetImageSize;
+    Methods[5] = GetImageRGBA;
+    Methods[6] = GetCSERIPPort;
+    Methods[7] = GetCurrentBatteryPower;
+    Methods[8] = GetAppID;
+    Methods[9] = SetOverlayNotificationPosition;
+    Methods[10] = IsAPICallCompleted;
+    Methods[11] = GetAPICallFailureReason;
+    Methods[12] = GetAPICallResult;
+    Methods[13] = RunFrame;
+    Methods[14] = GetIPCCallCount;
+    Methods[15] = SetWarningMessageHook;
+
+    Initialized = true;
+}
+void INTERFACE_STEAMUTILS004::Initialize()
+{
+    Methods = new void *[17];
+
+    Methods[0] = GetSecondsSinceAppActive;
+    Methods[1] = GetSecondsSinceComputerActive;
+    Methods[2] = GetConnectedUniverse;
+    Methods[3] = GetServerRealTime;
+    Methods[3] = GetIPCountry;
+    Methods[4] = GetImageSize;
+    Methods[5] = GetImageRGBA;
+    Methods[6] = GetCSERIPPort;
+    Methods[7] = GetCurrentBatteryPower;
+    Methods[8] = GetAppID;
+    Methods[9] = SetOverlayNotificationPosition;
+    Methods[10] = IsAPICallCompleted;
+    Methods[11] = GetAPICallFailureReason;
+    Methods[12] = GetAPICallResult;
+    Methods[13] = RunFrame;
+    Methods[14] = GetIPCCallCount;
+    Methods[15] = SetWarningMessageHook;
+    Methods[16] = IsOverlayEnabled;
+
+    Initialized = true;
+}
+void INTERFACE_STEAMUTILS005::Initialize()
+{
+    Methods = new void *[22];
+
+    Methods[0] = GetSecondsSinceAppActive;
+    Methods[1] = GetSecondsSinceComputerActive;
+    Methods[2] = GetConnectedUniverse;
+    Methods[3] = GetServerRealTime;
+    Methods[3] = GetIPCountry;
+    Methods[4] = GetImageSize;
+    Methods[5] = GetImageRGBA;
+    Methods[6] = GetCSERIPPort;
+    Methods[7] = GetCurrentBatteryPower;
+    Methods[8] = GetAppID;
+    Methods[9] = SetOverlayNotificationPosition;
+    Methods[10] = IsAPICallCompleted;
+    Methods[11] = GetAPICallFailureReason;
+    Methods[12] = GetAPICallResult;
+    Methods[13] = RunFrame;
+    Methods[14] = GetIPCCallCount;
+    Methods[15] = SetWarningMessageHook;
+    Methods[16] = IsOverlayEnabled;
+    Methods[17] = BOverlayNeedsPresent;
+    Methods[18] = CheckFileSignature;
+    Methods[19] = ShowGamepadTextInput;
+    Methods[20] = GetEnteredGamepadTextLength;
+    Methods[21] = GetEnteredGamepadTextInput;
+
+    Initialized = true;
+}
+void INTERFACE_STEAMUTILS006::Initialize()
+{
+    Methods = new void *[24];
+
+    Methods[0] = GetSecondsSinceAppActive;
+    Methods[1] = GetSecondsSinceComputerActive;
+    Methods[2] = GetConnectedUniverse;
+    Methods[3] = GetServerRealTime;
+    Methods[3] = GetIPCountry;
+    Methods[4] = GetImageSize;
+    Methods[5] = GetImageRGBA;
+    Methods[6] = GetCSERIPPort;
+    Methods[7] = GetCurrentBatteryPower;
+    Methods[8] = GetAppID;
+    Methods[9] = SetOverlayNotificationPosition;
+    Methods[10] = IsAPICallCompleted;
+    Methods[11] = GetAPICallFailureReason;
+    Methods[12] = GetAPICallResult;
+    Methods[13] = RunFrame;
+    Methods[14] = GetIPCCallCount;
+    Methods[15] = SetWarningMessageHook;
+    Methods[16] = IsOverlayEnabled;
+    Methods[17] = BOverlayNeedsPresent;
+    Methods[18] = CheckFileSignature;
+    Methods[19] = ShowGamepadTextInput;
+    Methods[20] = GetEnteredGamepadTextLength;
+    Methods[21] = GetEnteredGamepadTextInput;
+    Methods[22] = GetSteamUILanguage;
+    Methods[23] = IsSteamRunningInVR;
+
+    Initialized = true;
+}
+void INTERFACE_STEAMUTILS007::Initialize()
+{
+    Methods = new void *[24];
+
+    Methods[0] = GetSecondsSinceAppActive;
+    Methods[1] = GetSecondsSinceComputerActive;
+    Methods[2] = GetConnectedUniverse;
+    Methods[3] = GetServerRealTime;
+    Methods[3] = GetIPCountry;
+    Methods[4] = GetImageSize;
+    Methods[5] = GetImageRGBA;
+    Methods[6] = GetCSERIPPort;
+    Methods[7] = GetCurrentBatteryPower;
+    Methods[8] = GetAppID;
+    Methods[9] = SetOverlayNotificationPosition;
+    Methods[10] = IsAPICallCompleted;
+    Methods[11] = GetAPICallFailureReason;
+    Methods[12] = GetAPICallResult;
+    Methods[13] = RunFrame;
+    Methods[14] = GetIPCCallCount;
+    Methods[15] = SetWarningMessageHook;
+    Methods[16] = IsOverlayEnabled;
+    Methods[17] = BOverlayNeedsPresent;
+    Methods[18] = CheckFileSignature;
+    Methods[19] = ShowGamepadTextInput;
+    Methods[20] = GetEnteredGamepadTextLength;
+    Methods[21] = GetEnteredGamepadTextInput;
+    Methods[22] = GetSteamUILanguage;
+    Methods[23] = IsSteamRunningInVR;
+
+    Initialized = true;
+}
